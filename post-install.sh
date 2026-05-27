4313c68cc9d1e69b979e2718f10a4c2d3dd0b7d6
Geazi Soares <soares.geazi@hotmail.com>
2026-05-27 19:24:36 -0300

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,52 +1,37 @@
 #!/bin/bash
+
 set -e
 
-flatpak remote-delete fedora
-sleep 2
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sleep 2
-flatpak install -y flathub \
-app.drey.Dialect \
-be.alexandervanhee.gradia \
-ca.desrt.dconf-editor \
-com.brave.Browser \
-com.dec05eba.gpu_screen_recorder \
-com.github.finefindus.eyedropper \
-com.github.tchx84.Flatseal \
-com.mattjakeman.ExtensionManager \
-com.obsproject.Studio \
-com.protonvpn.www \
-com.spotify.Client \
-fr.handbrake.ghb \
-im.riot.Riot \
-io.github.celluloid_player.Celluloid \
-io.github.fabrialberio.pinapp \
-io.github.flattool.Ignition \
-io.github.flattool.Warehouse \
-io.github.getnf.embellish \
-io.github.josephmawa.TextCompare \
-io.github.onionware_github.onionmedia \
-io.github.thetumultuousunicornofdarkness.cpu-x \
-io.gitlab.news_flash.NewsFlash \
-io.missioncenter.MissionCenter \
-io.mpv.Mpv \
-io.typora.Typora \
-nl.hjdskes.gcolor3 \
-org.altlinux.Tuner \
-org.deluge_torrent.deluge \
-org.gimp.GIMP \
-org.gnome.Calculator \
-org.gnome.Evince \
-org.gnome.Loupe \
-org.gnome.font-viewer \
-org.gnome.meld \
-org.libreoffice.LibreOffice \
-org.mozilla.Thunderbird \
-org.mozilla.firefox \
-org.shotcut.Shotcut \
-org.telegram.desktop \
-org.videolan.VLC \
-page.codeberg.libre_menu_editor.LibreMenuEditor \
-page.tesk.Refine \
-xyz.tytanium.DoorKnocker
+# Troca remote Fedora pelo Flathub
+if flatpak remotes | grep -q '^fedora'; then
+    flatpak remote-delete fedora
+    sleep 2
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+fi
 
+# Instala Flatpaks do Flathub
+flatpak install -y flathub \
+    org.gtk.Gtk3theme.adw-gtk3 \
+    org.gtk.Gtk3theme.adw-gtk3-dark \
+    com.github.tchx84.Flatseal \
+    org.telegram.desktop \
+    com.google.Chrome \
+    org.mozilla.firefox \
+    com.dec05eba.gpu_screen_recorder \
+    com.discordapp.Discord \
+    com.spotify.Client \
+    com.thincast.client \
+    org.remmina.Remmina \
+    im.riot.Riot \
+    io.github.diegopvlk.Cine \
+    org.fedoraproject.MediaWriter\
+    org.gnome.Boxes \
+    org.gnome.Calculator \
+    org.gnome.TextEditor \
+    org.kde.gwenview \
+    page.codeberg.JakobDev.jdReplace \
+    page.codeberg.libre_menu_editor.LibreMenuEditor
+
+# Override global de tema escuro para todos os apps
+flatpak override --system --env=GTK_THEME=adw-gtk3-dark
+flatpak override --system --env=GTK_USE_PORTAL=1
